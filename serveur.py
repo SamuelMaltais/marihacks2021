7b3d081fff0ef9d52e130a9cb5bda3427f15f951
Samuel Maltais <leumasmontreal@hotmail.com>
2021-02-27 12:41:05 -0500

    basic fonctionality

--- serveur.py
+++ serveur.py
@@ -1,11 +1,57 @@
 from os import environ
 from flask import Flask, render_template
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SPREADSHEET_ID = '1to8XkoienC-2ITXDZUMfnC3p_mLUwUAU-_d3CKSk-00'
+RANGE_NAME = 'Tabellenblatt1'
+
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
-
 @app.route("/")
 def home():
-    return render_template(index.html)
+    print("something went right")
+    return "lmao !"
+
 @app.route("/getfoodbanks")
-def 
+def getfoodbanks():
+    return response
+@app.route("/oath2callback")
+def callback():
+    return "200"
+    
+def getspreadsheetinfo():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,range=RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+app.run(environ.get('PORT'))
