96231aba1f90b516af891f441fdc296813489a4b
Samuel Maltais <leumasmontreal@hotmail.com>
2021-02-28 02:03:44 -0500

    fully functionnal server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from os import environ
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, send_from_directory
 import pickle
 import os.path
 import gspread
@@ -9,6 +9,12 @@ import requests
 import googlemaps
 from apscheduler.schedulers.blocking import BlockingScheduler
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+import threading
+import time
+import schedule 
+
+
 #authentication with google API
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
@@ -17,8 +23,7 @@ client = gspread.authorize(creds)
 gmaps_key = googlemaps.Client(key = "AIzaSyA_ojPMG7H6WxeUH_nwv6xLrTF_QVVQNN0")
 #Flask app
 app = Flask(__name__)
-
-#SQL database
+#SQL database for all the foodbanks
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foodbanks.db'
 db = SQLAlchemy(app)
 class foodbanks(db.Model):
@@ -27,71 +32,105 @@ class foodbanks(db.Model):
     name = db.Column(db.String(50), unique=True, nullable="false")
     adress = db.Column(db.String(120), unique=True, nullable="false")
     lattitude = db.Column(db.Float(50), unique=True, nullable="false")
-   def __repr__(self):
+    longitude = db.Column(db.Float(50), unique=True, nullable="false")
+    uuid_ = db.Column(db.VARCHAR(36), unique=True, nullable="false")
+    def __repr__(self):
         return '<Name> %r>' % self.id
-
-#Response will be changed every 30sec so save needless spam
-response = []
-
-#base route
+#default route
 @app.route("/")
 def home():
-    return render_template("index.php")
+    return render_template("index.php", response=response)
 #Will return the current Json response
-@app.route("/getfoodbanks")
-def getfoodbanks():
-    return response
-
 @app.route("/random")
 def callback():
     return json.dumps(getspreadsheetinfo())
 
 #will change response every 30sec
 
-
-def getspreadsheetinfo():
-    #load sheet
-    sheet = client.open("Marihacks2021").sheet1
-    data = sheet.get_all_records()
-    response = []
-    #create new response body with lat, long and uuid
-    for obj in data:
-        food_bank_name = obj["Name"]
-        adress = obj["Address"]
-        #Check if we already fetched long amd lat from a certain foodbank, saves nedless requests
-        if adress in foodbanks.adress.query.all():
-            longitude = foodbanks.longitude.filter(foodbanks.name == food_bank_name).first()
-            lattitude = foodbanks.lattitude.filter(foodbanks.name == food_bank_name).first()
-        else:    
-        longitude, lattitude = get_lat_long(adress)
-        phone_number = obj["Phone"]
-        #Check if within a certain range from Montreal, about 56km
-        if(lattitude <= 45 or lattitude >= 46 or longitude <= -74 or longitude >= -73):
-            pass
-        else:
-            object1 = {
-            #    "id": uuid,
-                "name": food_bank_name,
-                "full_adress": adress,
-                "lat": lattitude,
-                "long": longitude,
-                "phoneNumber": phone_number
-            }
-            response.append(object1)
-    return response        
 def get_lat_long(adress):
     #make basic query to google maps
     data = gmaps_key.geocode(adress)
     latitude = data[0]['geometry']['location']['lat'] 
     longitude = data[0]['geometry']['location']['lng'] 
     return latitude, longitude
+def getspreadsheetinfo():
+    #load sheet
+    sheet = client.open("Marihacks2021").sheet1
+    data = sheet.get_all_records()
+    updated_list = []
+    #create new response body with lat, long and uuid
+    for obj in data:
+        food_bank_name = obj["Name"]
+        adress = obj["Address"]
+        phone_number = obj["Phone"]
+        #Check if we already fetched long amd lat from a certain foodbank, saves nedless requests
+        if foodbanks.query.filter_by(name=food_bank_name).count() == 1:
+            foodbank_data = foodbanks.query.filter_by(name=food_bank_name).first()
+            uuid_ = foodbank_data.uuid_
+            longitude = foodbank_data.longitude
+            latitude = foodbank_data.lattitude
+            print(longitude, latitude, "Entry already present")
+        else:    
+            uuid_ = uuid.uuid4()
+            latitude, longitude = get_lat_long(adress)
+            try:
+                test_foodbank = foodbanks(name=food_bank_name, adress=adress,lattitude=latitude,longitude=longitude, uuid_ = str(uuid_))
+                db.session.add(test_foodbank)
+                db.session.commit()
+                print("worked")
+            except:
+                db.session.rollback()
+                print("unlucky")
+                raise
+            finally:
+                db.session.close()
+        #Check if within a certain range from Montreal, about 56km
+        if(latitude <= 45 or latitude >= 46 or longitude <= -81 or longitude >= -73):
+            print("Outside of Montreal")
+            pass
+        else:
+            object1 = {
+                "id": str(uuid_),
+                "name": food_bank_name,
+                "full_adress": adress,
+                "lat": latitude,
+                "long": longitude,
+                "phoneNumber": phone_number
+            }
+            updated_list.append(object1)
+    return updated_list
+#Response will be changed every 30sec to save needless spam
+response = getspreadsheetinfo()
+
+@app.route("/getfoodbanks")
+def getfoodbanks():
+    return json.dumps(response)
+#This will change the response value every 30sec on a different thread
+def run_continuously(interval=1):
+    cease_continuous_run = threading.Event()
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while not cease_continuous_run.is_set():
+                schedule.run_pending()
+                time.sleep(interval)
+    continuous_thread = ScheduleThread()
+    continuous_thread.start()
+    return cease_continuous_run
+
+def background_job():
+    response = getspreadsheetinfo()
+    print("response updated")
+schedule.every(10).seconds.do(background_job)
+# Start the background thread
+stop_run_continuously = run_continuously()      
+
 @app.route("/editor")
 def editor():
-    render_template("editor.php")
-
+    return render_template("editor.php")
 @app.route("/inventory")
 def inventory():
-    render_template("inventory.php")
+    return render_template("inventory.php")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
--- templates/editor.php
+++ templates/editor.php
@@ -1,5 +1,125 @@
 <!DOCTYPE html>
 <html lang="en">
+<style>
+    *{
+    padding: 0;
+    margin: 0;  
+    box-sizing: border-box;  
+}
+body{
+    background-color: #f8f9fa;
+}
+#map{
+    margin: 2rem auto;
+    width: 70%;
+    height: 100%;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}  
+  li {
+    float: left;
+  }
+  
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  li a:hover {
+    background-color: #111;
+  }  
+#locations{
+    display: block;
+    text-align: center;
+    padding: 1rem;
+}
+#locations div{
+    font-size: 1.5rem;
+    margin: 1rem auto;
+    width: 60%;
+    color: #fff;
+    background-color: #333;
+    padding: 2rem;
+}
+#first{
+  margin: 1rem;
+}
+.dropbtn {
+  background-color: #333;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #ddd;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {display: block;}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {background-color: rgb(80, 80, 80);}
+
+#submit a{
+  display: inline-block;
+  text-decoration: none;
+  color: #fff;
+  margin: 10rem 1rem 1rem 1rem;
+  padding: 1em;
+  border-radius: 10px;
+  background-color: #333;
+}
+#item{
+  display: inline-block;
+  color: #fff;
+  background-color: #333;
+  padding: 2rem;
+  margin: 2rem 0 0 2%;
+}
+form p{
+  margin-bottom: 0.5rem;
+}
+footer{
+    color: #fff;
+    background-color: #333;
+    text-align: center;
+    padding: 0.5em;
+    bottom: 0;
+    position: absolute;
+}
+</style>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
